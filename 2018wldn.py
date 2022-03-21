b6ae0a325cba0ccb73f4fa320e921ab1e3c3b87f
ch130812 <100828787+ch130812@users.noreply.github.com>
2022-03-21 15:55:25 +0900

    Add files via upload

---
+++ 2018wldn.py
@@ -0,0 +1,10 @@
+## 함수 선언부분##
+def myFunc():
+    print('함수를 호출함.')
+
+#전역 변수 선언 부분 ##
+gVar = 100
+if __name__ == '__main__':
+    print('메인 함수 부분이 실행 됩니다.')
+    myFunc()
+    print('전역 변수 값:',gVar)
